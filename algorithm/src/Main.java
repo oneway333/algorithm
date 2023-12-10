a20f9d77dd8b926fd0aedbd8f52e51cd9813ad47
oneway333 <onew6465@gmail.com>
2023-12-10 16:03:35 +0800

    分支应该不是这么用的呀。。。

--- algorithm/src/Main.java
+++ algorithm/src/Main.java
@@ -2,5 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("fuck you");
+        Main main = new Main();
+        int i;
+        for (i = 2; i < 5; i++)
+            System.out.println(main.fun(i));
+    }
+
+    int fun(int n){
+        if (n == 1) return 1;
+        return n * fun(n - 1);
     }
 }
