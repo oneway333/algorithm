49bc0ec0322440aa7f7cfad579b96f663fca22fa
oneway333 <onew6465@gmail.com>
2023-12-11 03:34:59 +0800

    果然敲了几遍就越来越熟悉了，再敲个十几遍那还得了！不幻想了，早点睡吧~~~

--- C_Learning/main.c
+++ C_Learning/main.c
@@ -1,71 +1,76 @@
+/*
+ *
+3.7 节点交换
+给定一个链表的头指针head，要求两两交换链表中相邻的节点。
+输入：head = [1,2,3,4]
+输出：[2,1,4,3]
+输入：head = [1,2,3,4,5]
+输出：[2,1,4,3,5]
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define OK 1
-#define ERROR 0
-#define OVERFLOW 0
+typedef struct node{
+    int n;
+    struct node *next;
+} Node;
 
-typedef int status;
-typedef int qElemType;
-
-// 其实主要逻辑没啥难理解
-
-typedef struct qNode {
-    qElemType data;
-    struct qNode *next;
-} QNode, *QueuePtr; // QNode是独立的结点，QueuePtr是指向结点的指针
-
-typedef struct {
-    QueuePtr front, rear; // 指向队列头和尾的指针
-}LinkQueue;
-
-// 入队
-status enQueue(LinkQueue *lq, qElemType e) {
-    QueuePtr t = (QueuePtr)malloc(sizeof(QNode));
-    if (!t) exit(OVERFLOW);
-    t->data = e;
-    t->next = NULL;
-    lq->rear->next = t; // 总结来说就是尾插法，lq的rear一开始指向头结点
-    lq->rear = t;
-    return OK;
+Node * newNode(int n){
+    Node *node = (Node *)malloc(sizeof(Node));
+    node->n = n;
+    node->next = NULL;
+    return node;
 }
 
-// 出队
-status deQueue(LinkQueue *lq, qElemType *e){
-    if (lq->rear == lq->front) return ERROR; // 队列没有元素
-    QueuePtr p = lq->front->next;
-    // 队列只有一个元素，必须这么做，不然rear被free之后，就变成野指针了，再次进行enQueue的时候，就会有lq->rear为NULL的问题
-    if (p == lq->rear) lq->rear = lq->front;
-    *e = p->data;
-    lq->front->next = p->next;
-    free(p);
-    return OK;
+Node *createList(int nums[], int n){
+    Node *head = NULL;
+    Node *p = head;
+    for (int i = 0; i < n; ++i) {
+        Node *t = newNode(nums[i]);
+        if (head == NULL) head = t;
+        else p->next = t;
+        p = t;
+    }
+    return head;
 }
 
-void show(LinkQueue *lq){
-    QueuePtr p = lq->front->next;
-    while (p) {
-        printf("%d\t", p->data);
-        p = p->next;
+void show(Node *head){
+    while (head != NULL){
+        printf("%d\t", head->n);
+        head = head->next;
     }
 }
 
+Node *swap(Node *head) {
+    if (head == NULL) return NULL;
+    if (head->next == NULL) return head;
+    Node *p = head->next;
+    head->next = swap(p->next);
+    p->next = head;
+    return p;
+}
+
+Node *delete(Node *head, int k){
+    if (head == NULL) return NULL;
+    head->next = delete(head->next, k);
+    return head->n == k ? head->next : head;
+}
+
+Node *delete2(Node *head){
+    if (head == NULL) return NULL;
+    if (head->next == NULL) return head;
+    head->next = delete2(head->next);
+    return head->next->n > head->n ? head->next : head;
+}
+
 int main(){
-    LinkQueue lq;
-    QueuePtr p = (QueuePtr)malloc(sizeof(QNode));
-    p->data = 0;
-    p->next = NULL;
-    lq.front = p;
-    lq.rear = p;
-    int i, e;
-    for (i = 0; i < 10; i++) enQueue(&lq, i + 1);
-    show(&lq);
-    putchar('\n');
-    deQueue(&lq, &e);
-    show(&lq);
-    putchar('\n');
-    printf("%d\n", e);
-    enQueue(&lq, 100);
-    show(&lq);
+    int nums[] = {5,2,13,3,8};
+    Node *head = createList(nums, sizeof(nums) / sizeof(int));
+
+//    show(swap(head));
+//    show(delete(head, 7));
+    show(delete2(head));
     return 0;
+
 }
