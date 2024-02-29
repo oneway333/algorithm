fafa9eda8a29a217423bdabda82d90c80070292c
oneway333 <onew6465@gmail.com>
2024-02-29 13:43:11 +0800

    正式开始vue

---
+++ vue/Vue/19_单文件组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <School></School>
+    <Student></Student>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import School from "./School.vue";
+import Student from "./Student.vue";
+
+export default {
+  name: "App",
+  // 将组件放入components中
+  components: {
+    School,
+    Student,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ vue/Vue/19_单文件组件/School.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>School {{name}}</h2>
+        <h2>School {{addr}}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "School",    // 组件的名称
+    data(){
+        return {
+            name: 'fuck',
+            addr: 'shit'
+        }
+    }
+};
+</script>
+
+<style>
+</style>
